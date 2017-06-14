d20b4265b6cd856ec2cd5adc146aea1762037dee
Allan Liebold <allan.liebold@gmail.com>
2017-06-14 11:57:33 -0700

    creates prototype functions for Project object and should append the object data to the page, but is not working properly yet.

---
+++ js/projects.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var rawData = [
+  {
+    title: 'DunGen & Dragons',
+    projUrl: 'https://loaye.github.io/DunGen-Dragons/home/index.html',
+    createdOn: '2017-06-02',
+    body: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>'
+  },
+  {
+    title: 'Filler Project',
+    projUrl: '#',
+    createdOn: '2017-06-14',
+    body: '<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>'
+  }
+];
