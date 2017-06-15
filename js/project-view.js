51c6531535726ae8e5c30754ccc348b093498e75
Allan Liebold <allan.liebold@gmail.com>
2017-06-15 10:15:01 -0700

    creates handlers for hamburger menu. Nav menu displays as box in corner on mouse enter and disappears on mouseleave

--- js/project-view.js
+++ js/project-view.js
@@ -3,7 +3,10 @@
 var projectView = {};
 
 projectView.handleNav = function() {
-  $('nav').on('click', 'li.tab', function() {
+  $('nav').on('click', 'li.tab', function(c) {
+    c.preventDefault();
+    $('.tab').removeClass('clicked');
+    $(this).addClass('clicked');
     $('.tab-content').hide();
     var $tab = $(this).data('content');
     $('#' + $tab).show();
@@ -12,6 +15,17 @@ projectView.handleNav = function() {
   $('nav .tab:nth-child(2)').click();
 };
 
+projectView.showNav = function() {
+  $('nav').on('mouseover', 'span', function() {
+    $('nav ul').toggleClass('shown');
+  });
+
+  $('nav ul').on('mouseleave', function() {
+    $(this).removeClass('shown');
+  });
+}
+
 $(document).ready(function () {
   projectView.handleNav();
+  projectView.showNav();
 })
